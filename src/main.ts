f59513e04eda0767a99b4f83ddda349ed6d9f17a
Mercury <mercuryhg@airmail.cc>
2018-08-08 06:04:52 +0200

    Some edits for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { createServer } from "http";
 import Commands from "./commands";
 import Events from "./events";
 
 // Initialize functions
 Commands();
 Events();
+
+// Heroku sucks, make a simple HTTP server to make him happy
+createServer(((req, res) => res.end())).listen(process.env.PORT);
