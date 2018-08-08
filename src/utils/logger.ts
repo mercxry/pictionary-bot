f7bc068185b476bc484ff257b495958a186fe022
Mercury <mercuryhg@airmail.cc>
2018-08-08 17:39:27 +0200

    Small changes to the date on logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,16 +1,16 @@
 import chalk from "chalk";
 
 export const logError = (error: string) => {
-  const date: string = new Date().toString();
+  const date: string = new Date().toISOString();
   console.error(`${chalk.grey(date)} | ${chalk.bold.red("[ERROR]")} ${chalk.redBright(error)}`);
 };
 
 export const logWarning = (warning: string) => {
-  const date: string = new Date().toString();
+  const date: string = new Date().toISOString();
   console.warn(`${chalk.grey(date)} | ${chalk.bold.yellow("[WARNING]")} ${chalk.yellowBright(warning)}`);
 };
 
 export const logInfo = (info: string) => {
-  const date: string = new Date().toString();
+  const date: string = new Date().toISOString();
   console.info(`${chalk.grey(date)} | ${chalk.bold.cyan("[INFO]")} ${chalk.cyanBright(info)}`);
 };
